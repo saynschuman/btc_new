dcb18d579cab931a718852582826057552075a9a
saynschuman <saynschuman@gmail.com>
2018-11-13 20:14:12 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { Router, Link } from '@reach/router'
+import Investor from './pages/investor'
+import Admin from './pages/admin'
+import Home from './pages/home'
 
 class App extends Component {
   render() {
-    return <div>Hello app</div>
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+            <li>
+              <Link to="/investor">Инвестор</Link>
+            </li>
+            <li>
+              <Link to="/admin">Админ</Link>
+            </li>
+          </ul>
+        </nav>
+        <Router>
+          <Home default />
+          <Investor path="/investor" />
+          <Admin path="/admin" />
+        </Router>
+      </div>
+    )
   }
 }
 
