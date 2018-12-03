5c3a4a3db06ce2042496793f35f76abc051723f7
Vitaliy <saynschuman@gmail.com>
2018-12-03 23:34:27 +0200

    new app created

--- src/store/reducer/index.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"
-import IStore from '../storeTypes'
-import loginInvestor from '../modules/loginInvestor'
-
-export default combineReducers<IStore>({
-    loginInvestor
-})
