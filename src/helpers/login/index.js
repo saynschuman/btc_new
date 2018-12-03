b79c5dec4687f93114f741c8740d6c69c4faff5a
Vitaliy <saynschuman@gmail.com>
2018-12-04 01:06:51 +0200

    statemennt fixed

--- src/helpers/login/index.js
+++ src/helpers/login/index.js
@@ -1,6 +1,7 @@
 import Cookies from "universal-cookie"
 
 export const investorLogin = () => {
+  // statement fixed
   const cookies = new Cookies()
   cookies.set("token", "111", { expires: new Date(Date.now() + 1000 * 3) })
   window.location.replace("/investor")
