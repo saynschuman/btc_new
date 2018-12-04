47a75bf7a9c69cee13ad16cd21d665a336b7d840
Vitaliy <saynschuman@gmail.com>
2018-12-04 08:26:46 +0200

    header & sidebar finished

--- src/components/common/InvestorHeader/index.js
+++ src/components/common/InvestorHeader/index.js
@@ -2,14 +2,44 @@ import React, { Component } from "react"
 import css from "./index.module.scss"
 import { Link } from "@reach/router"
 import Cookies from "universal-cookie"
+import classNames from "classnames"
+
+const languages = [
+  {
+    label: "RU",
+    value: "RU"
+  },
+  {
+    label: "EN",
+    value: "EN"
+  }
+]
 
 class Header extends Component {
+  state = {
+    language: "RU",
+    langIsOpen: false
+  }
   exit = () => {
     const cookies = new Cookies()
     cookies.remove("token")
     window.location.replace("/")
   }
+  chooseLang = () => {
+    this.setState({
+      langIsOpen: !this.state.langIsOpen
+    })
+  }
+  chooseThisLang = lang => {
+    this.setState({
+        language: lang
+    })
+    this.chooseLang()
+  }
   render() {
+    const isActive = ({ isCurrent }) => {
+      return isCurrent ? { className: "active-link" } : null
+    }
     return (
       <div className={css.headerWrapper}>
         <div className={css.headerInner}>
@@ -20,21 +50,49 @@ class Header extends Component {
             <nav className={css.nav}>
               <ul>
                 <li>
-                  <Link to="">Главная</Link>
+                  <Link getProps={isActive} {...this.props} to="">
+                    Главная
+                  </Link>
                 </li>
                 <li>
-                  <Link to="buy">Покупка-продажа</Link>
+                  <Link getProps={isActive} {...this.props} to="buy">
+                    Покупка-продажа
+                  </Link>
                 </li>
                 <li>
-                  <Link to="settings">Настройки</Link>
+                  <Link getProps={isActive} {...this.props} to="settings">
+                    Настройки
+                  </Link>
                 </li>
               </ul>
             </nav>
           </div>
           <div className={css.right}>
             <ul>
-              <li className={css.lang}>
-                <span>RU</span>
+              <li
+                className={classNames({
+                  [css.lang]: true,
+                  [css.active]: this.state.langIsOpen
+                })}
+              >
+                <span onClick={this.chooseLang}>{this.state.language}</span>
+                {this.state.langIsOpen && (
+                  <div className={css.toggleLang}>
+                    {languages.map((lang, index) => {
+                      return (
+                        <div
+                          key={index}
+                          onClick={() => this.chooseThisLang(lang.value)}
+                          className={classNames({
+                            [css.chooseLang]: true
+                          })}
+                        >
+                          {lang.value}
+                        </div>
+                      )
+                    })}
+                  </div>
+                )}
               </li>
               <li className={css.exit}>
                 <span onClick={this.exit}>Выход</span>
