9880e2bea961449342928178aeb9e35355ad36c9
Vitaliy <saynschuman@gmail.com>
2018-12-05 05:17:53 +0200

    table fixed

--- src/components/common/general/CustomTable/index.js
+++ src/components/common/general/CustomTable/index.js
@@ -1,11 +1,9 @@
 import React from "react"
+import ReactTable from "react-table"
+import "react-table/react-table.css"
 
-const Index = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Index = props => {
+  return <ReactTable data={props.data} columns={props.columns} />
 }
 
 export default Index
--- src/components/pages/investor/root/buy/index.js
+++ src/components/pages/investor/root/buy/index.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React from "react"
+import css from "./index.module.scss"
+import classNames from "classnames"
 
-const Index = () => {
+class Settings extends React.Component {
+  state = {
+    investors: true,
+    investitions: false
+  }
+  handleTabs = () => {
+    this.setState({
+      investors: !this.state.investors,
+      investitions: !this.state.investitions
+    })
+  }
+  render() {
     return (
-        <div>
-            buy
+      <div>
+        <div className={css.tabs}>
+          <div
+            onClick={this.handleTabs}
+            className={classNames({
+              "not-active": !this.state.investors
+            })}
+          >
+            Активные инвестиции
+          </div>
+          <div
+            onClick={this.handleTabs}
+            className={classNames({
+              "not-active": !this.state.investitions
+            })}
+          >
+            История инвестиций
+          </div>
         </div>
-    );
-};
+        {this.state.investors && (
+          <table className={css.tableHeader}>
+            <tbody>
+              <th>Дата входа</th>
+              <th>Срок</th>
+              <th>Сумма TH/s</th>
+              <th>Стоимость до конца срока (?)</th>
+              <th>Коэфициент начисления</th>
+              <th>Выплачено</th>
+              <th>Продажа (?)</th>
+            </tbody>
+          </table>
+        )}
+        {this.state.investitions && (
+          <table className={css.tableHeader}>
+            <tbody>
+              <th>Дата</th>
+              <th>Срок</th>
+              <th>Коэфициент начисления</th>
+              <th>Операция</th>
+              <th>Сумма BTC</th>
+              <th>Сумма TH/s</th>
+              <th>Статус (?)</th>
+            </tbody>
+          </table>
+        )}
+      </div>
+    )
+  }
+}
 
-export default Index;
+export default Settings
