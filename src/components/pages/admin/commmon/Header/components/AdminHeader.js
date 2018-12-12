930b2464a057fb8ed25ea4571a0c6001ac33f9aa
Vitaliy <saynschuman@gmail.com>
2018-12-12 03:47:51 +0200

    slice bug fixed

--- src/components/pages/admin/commmon/Header/components/AdminHeader.js
+++ src/components/pages/admin/commmon/Header/components/AdminHeader.js
@@ -1,22 +1,32 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react"
+import PropTypes from "prop-types"
 import { parseJwt } from "../../../../../../helpers/parseJwt"
 
 class AdminHeader extends Component {
   static propTypes = {
     isAdmin: PropTypes.bool
-  };
+  }
+  renderId = () => {
+    if (
+      localStorage.getItem("token") !== null &&
+      localStorage.getItem("token").length > 0
+    ) {
+      parseJwt(localStorage.getItem("token")).id.slice(0, 7)
+    } else {
+      return
+    }
+  }
   render() {
     return (
       <div className="admin-header">
         <ul>
           <li>Панель администратора</li>
           <li />
-          <li>ID: {parseJwt(localStorage.getItem("token")).id.slice(0, 7)}</li>
+          <li>ID: {this.renderId()}</li>
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default AdminHeader;
+export default AdminHeader
